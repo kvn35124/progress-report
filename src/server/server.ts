26214ce2ea4366b0a4daa31049fcbedbed5811bc
Kevin Auer <kvn35124@uab.edu>
2020-03-10 16:35:28 -0500

    initial commit

---
+++ src/server/server.ts
@@ -0,0 +1,10 @@
+import * as express from 'express';
+
+
+const app = express();
+
+app.use(express.static('public'));
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server listening on port: ${port}`));
